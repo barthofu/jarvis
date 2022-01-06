8088541929f6c8be85094fed472178ae9d02acb6
Bartholomé <bartho.gili@gmail.com>
2022-01-06 23:18:53 +0100

    feat(module): DisplayMessage

---
+++ src/modules/display_message.py
@@ -0,0 +1,21 @@
+from src.core.task import Task 
+
+from sense_hat import SenseHat
+
+class DisplayMessageTask(Task):
+    
+    def __init__(self):
+        super().__init__([
+            'display the message'
+        ])
+
+        self.sense = SenseHat()
+        
+    def action(self, text):
+
+        message = ' '.join(text.split('message'))
+        
+        self.sense.show_message(message)
+        
+        return ''
+        
