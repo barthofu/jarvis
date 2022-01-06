18494ac4d8b24a121667f9cc086272e555966adf
Bartholomé <bartho.gili@gmail.com>
2022-01-06 23:16:36 +0100

    feat(module): Lights

---
+++ src/modules/lights.py
@@ -0,0 +1,36 @@
+from src.core.task import Task 
+
+from sense_hat import SenseHat
+
+class LightsOnTask(Task):
+    
+    def __init__(self):
+        super().__init__([
+            'turn on lights',
+            'switch on lights'
+        ])
+
+        self.sense = SenseHat()
+        
+    def action(self, text):
+        
+        self.sense.clear(255, 255, 255)
+        return 'Okay, I turn on the lights'
+
+
+
+class LightsOffTask(Task):
+    
+    def __init__(self):
+        super().__init__([
+            'turn off lights',
+            'switch off lights'
+        ])
+
+        self.sense = SenseHat()
+        
+    def action(self, text):
+        
+        self.sense.clear()
+        return 'Okay, I turn off the lights'
+        
