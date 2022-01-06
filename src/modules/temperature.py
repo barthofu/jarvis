ee7b69f3d35eb6e0978af2dd2cd64071f51b5cd6
Bartholomé <bartho.gili@gmail.com>
2022-01-06 23:22:20 +0100

    feat(module): Temperature

---
+++ src/modules/temperature.py
@@ -0,0 +1,18 @@
+from src.core.task import Task 
+
+from sense_hat import SenseHat
+
+class CurrentTemparatureTask(Task):
+    
+    def __init__(self):
+        super().__init__([
+            'what is temperature',
+            'current temperature'
+        ])
+
+        self.sense = SenseHat()
+        
+    def action(self, text):
+        
+        temp = self.sense.get_temperature()
+        return f"Currently, it is {temp} degrees"
